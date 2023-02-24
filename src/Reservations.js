43f9541716176bf5730c64d08955252dfefe4952
ultravioletg <ultravioletrad@tutanota.com>
2023-02-24 23:47:43 +0500

    Added navigation to Reservations on buttons to reserve a table

--- src/Reservations.js
+++ src/Reservations.js
@@ -4,10 +4,10 @@ import './Reservations.css'
 function Reservations(){
     return(
         <div className="bookingPage">
-            <div className="bookingform">
+         <div className="bookingform">
               <h1>For reservation fill the form below!</h1>  
-            <BookingPage/>
-        </div>
+              <BookingPage/>
+          </div>
         </div>
     );
 
